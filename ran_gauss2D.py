e71e38299f58e5fbc6b557c0cc3d6f7421b2a3d3
Florian Beutler <fbeuter@lbl.gov>
2015-06-02 16:57:53 -0700

    Update ran_gauss2D.py

--- ran_gauss2D.py
+++ ran_gauss2D.py
@@ -62,42 +62,6 @@ def gen_2Dgauss(N, Lx, Ly, model):
     
     return field
 
-def calc_power(N, field, dk, Nshot, Lx, Ly, norm):
-    
-    dkx = (2.*np.pi)/Lx;
-    dky = (2.*np.pi)/Ly;
-    V = Lx*Ly
-    
-    power = np.zeros(N, dtype = float)
-    Nmodes = np.zeros(N, dtype = int)
-    
-    for ix in range(0, N):
-        if ix <= N/2:
-            kx = ix*dkx
-        else:
-            kx = (ix-N)*dkx
-        
-        for iy in range(0, N):
-            if iy <= N/2:
-                ky = iy*dky
-            else:
-                ky = (iy-N)*dky
-            
-            kval = (kx**2 + ky**2)**0.5
-            
-            if kval > 0:
-                power[int(kval/dk)] = power[int(kval/dk)] + field[ix][iy].real**2 + field[ix][iy].imag**2 - Nshot
-                Nmodes[int(kval/dk)] = Nmodes[int(kval/dk)] + 1
-    
-    iNonZeros = np.where(Nmodes != 0)
-    iZeros = np.where(Nmodes == 0)
-    power[iNonZeros] = power[iNonZeros]/Nmodes[iNonZeros]
-    
-    k = np.linspace(dkx/2., (N-1)*dkx + dkx/2., num=N)
-    k[iZeros] = 0.
-    
-    return V*np.trim_zeros(power)/norm, np.trim_zeros(k)
-
 def main():
     
     Lx = 1.
@@ -111,16 +75,6 @@ def main():
     #-----------------------------------------#
     
     kspace_field = gen_2Dgauss(N, Lx, Ly, model0)
-    
-    power, k = calc_power(N, kspace_field, dk, 0., Lx, Ly, 1.)
-    
-    pl.title("power")
-    pl.xlabel("k")
-    pl.ylabel("P(k)")
-    pl.xscale("log", nonposy="clip")
-    pl.plot(k, model0(k), 'b', linewidth=2)
-    pl.plot(k, power, 'r', linewidth=2)
-    pl.show()
     
     config_field = np.fft.ifft2(kspace_field)*kspace_field.size**0.5
     
